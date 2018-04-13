08ba74762701105ea39a4b96e0e0c4ea3ad4ebd0
Adam Huang <adamhut@gmail.com>
2018-04-13 07:38:44 -0700

    adjust pikaday

--- resources/assets/js/components/Pikaday.vue
+++ resources/assets/js/components/Pikaday.vue
@@ -1,9 +1,24 @@
 <template>
    <div class="min-h-screen text-black bg-grey-lighter p-8">
     <div class="bg-white rounded shadow-lg max-w-md mx-auto py-8 px-4">
-        <pikaday-item v-model="adate">
-            <input type="text" class="mr-3" v-model="adate">
+        <pikaday-item v-model="form.body" :first-day="firstDay">
+            <input type="text" class="bg-purple-white shadow rounded border-1 p-1" v-model="form.body">
         </pikaday-item>
+       
+        <div class="relative mr-6 my-2">
+        <input type="search" class="bg-purple-white shadow rounded border-0 p-3" placeholder="Search by name...">
+        <div class="absolute pin-r pin-t mt-3 mr-4 text-purple-lighter">
+            <svg version="1.1" class="h-4 text-dark" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 52.966 52.966" style="enable-background:new 0 0 52.966 52.966;" xml:space="preserve">
+                <path d="M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21
+                c5.083,0,9.748-1.817,13.384-4.832l14.895,15.491c0.196,0.205,0.458,0.307,0.721,0.307c0.25,0,0.499-0.093,0.693-0.279
+                C52.074,52.304,52.086,51.671,51.704,51.273z M21.983,40c-10.477,0-19-8.523-19-19s8.523-19,19-19s19,8.523,19,19
+                S32.459,40,21.983,40z"/>
+
+            </svg>
+
+        </div>
+        </div>
     </div>
    </div>
 </template>
@@ -13,17 +28,19 @@
     export default {
         data() {
             return {
-                adate: '2018-04-12'
+                firstDay: 0,
+                form:{
+                    body:'',
+                },
             }
         },
         components:{
             PikadayItem,
         },
+        
+
+
 
 
     }
 </script>
-
-<style scoped>
-
-</style>
--- resources/assets/js/components/PikadayItem.vue
+++ resources/assets/js/components/PikadayItem.vue
@@ -4,44 +4,104 @@
     //import Pikaday from 'Pikaday'
     export default {
          props:{
+            minDate: {},
+            maxDate: {},
+            defaultDate: {},
             value:{
                 require:true,
             },
+            format: {
+                default: 'YYYY-MM-DD',
+                type: String
+            },
+            firstDay: {
+                default: 1,
+                type: Number
+            },
+            calendarIcon: {
+                default: true,
+                type: Boolean
+            },
+            itemClass:{
+                    default:'date-picker'
+            },
+            
         },
         data() {
             return {
-                item: this.value,
+               // item: this.value,
+
+               currentDate:'',
             }
         },
-        
+         watch: {
+            minDate (newMinDate) {
+                this.picker.setMinDate(newMinDate)
+                if (this.currentDate < newMinDate) {
+                    this.picker.setDate('1970-01-01', true)
+                }
+            },
+            maxDate (newMaxDate) {
+                this.picker.setMaxDate(newMaxDate)
+                if (this.currentDate > newMaxDate) {
+                    this.picker.setDate(newMaxDate)
+                }
+            }
+        },
         mounted(){
-            let picker = new Pikaday({
+           this.picker = new Pikaday({
                 field: this.$el,
+                format: this.format,
+                firstDay:this.firstDay,
                 onSelect: (date)=>{
-                    console.log(picker.getMoment().format('YYYY-MM-DD'));
+                   // console.log(date),
+                    this.currentDate = this.picker.getMoment().toDate();
+                    //console.log(picker.getMoment().format('YYYY-MM-DD'));
                     // /this.$emit('input');
                     // /field.value = picker.getMoment().toString();
-                    this.item = picker.getMoment().format('YYYY-MM-DD');
-                    this.$emit('input', this.item);
+                    //this.item =;
+                    this.$emit('input',  this.picker.getMoment().format('YYYY-MM-DD'));
                 },
             })
-
-
+            this.currentDate = this.defaultDate;
+            this.$el.classList.add(this.itemClass);
+            if(this.calendarIcon)
+            {
+                 this.$el.classList.add('calendar-icon');
+            }
         },
     
         render(){        
             return this.$slots.default[0]   //
-
-           /* return this.$scopedSlots.default({
+            /*
+            return this.$scopedSlots.default({
                 items:this.value,
                 //itemClass :this.itemClass,
                 //handleClass :this.handleClass
             });
-            */
+            */           
         }
     }
 </script>
 
+<style src="pikaday/css/pikaday.css"></style>
 <style scoped>
-
+input.date-picker.calendar-icon {
+  background-image: url(/static/calendar.png);
+  background-repeat: no-repeat;
+  background-position: right;
+}
+.date-picker {
+ /* width: 150px;
+  margin-right: 0;
+  margin-left: 0;
+  border: 1px solid #ccc;
+  padding: 9px 0;
+  line-height: 1.1;
+  color: #444;
+  border-radius: 3px;
+  text-indent: 13px;
+  cursor: pointer;
+  */
+}
 </style>
