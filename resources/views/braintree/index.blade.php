f08781f7dbe7a0646fc6b72f1a4eee1e7c12041d
Adam Huang <adamhut@gmail.com>
2019-03-16 09:00:32 -0700

    wip

--- resources/views/braintree/index.blade.php
+++ resources/views/braintree/index.blade.php
@@ -1,18 +1,19 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <title>Laravel</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
@@ -63,85 +64,87 @@
                 margin-bottom: 30px;
             }
         </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                @if (session('success_message'))
-                    <div class="alert alert-success">
-                        {{ session('success_message') }}
-                    </div>
-                @endif
+</head>
 
-                @if(count($errors) >0)
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            
-                        @endforeach
-                    </ul>
-                    </div>
-                @endif
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            @if (session('success_message'))
+            <div class="alert alert-success">
+                {{ session('success_message') }}
             </div>
-            <div class="content">
-                <div class="title m-b-md">
-                    
-                <form method="post" id="payment-form" action="{{ route('braintree.store') }}">
-                        @csrf
-                        <section>
-                            <label for="amount">
-                                <span class="input-label">Amount</span>
-                                <div class="input-wrapper amount-wrapper">
-                                    <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
-                                </div>
-                            </label>
+            @endif
 
-                            <div class="bt-drop-in-wrapper">
-                                <div id="bt-dropin"></div>
-                            </div>
-                        </section>
+            @if(count($errors) >0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
 
-                        <input id="nonce" name="payment_method_nonce" type="hidden" />
-                        <button class="button" type="submit"><span>Test Transaction</span></button>
-                    </form>
-                    
-                </div>
-
-               
+                    @endforeach
+                </ul>
             </div>
+            @endif
         </div>
-         <script src="https://js.braintreegateway.com/web/dropin/1.16.0/js/dropin.min.js"></script>
+        <div class="content">
+            <div class="title m-b-md">
+
+                <form method="post" id="payment-form" action="{{ route('braintree.store') }}">
+                    @csrf
+                    <section>
+                        <label for="amount">
+                            <span class="input-label">Amount</span>
+                            <div class="input-wrapper amount-wrapper">
+                                <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
+                            </div>
+                        </label>
+
+                        <div class="bt-drop-in-wrapper">
+                            <div id="bt-dropin"></div>
+                        </div>
+                    </section>
+
+                    <input id="nonce" name="payment_method_nonce" type="hidden" />
+                    <button class="button" type="submit"><span>Test Transaction</span></button>
+                </form>
+
+            </div>
+
+
+        </div>
+    </div>
+    <script src="https://js.braintreegateway.com/web/dropin/1.16.0/js/dropin.min.js"></script>
     <script>
         var form = document.querySelector('#payment-form');
         var client_token = "{{ $braintreeToken }}";
 
         braintree.dropin.create({
-          authorization: client_token,
-          selector: '#bt-dropin',
-          paypal: {
-            flow: 'vault'
-          }
+            authorization: client_token,
+            selector: '#bt-dropin',
+            paypal: {
+                flow: 'vault'
+            }
         }, function (createErr, instance) {
-          if (createErr) {
-            console.log('Create Error', createErr);
-            return;
-          }
-          form.addEventListener('submit', function (event) {
-            event.preventDefault();
-
-            instance.requestPaymentMethod(function (err, payload) {
-              if (err) {
-                console.log('Request Payment Method Error', err);
+            if (createErr) {
+                console.log('Create Error', createErr);
                 return;
-              }
+            }
+            form.addEventListener('submit', function (event) {
+                event.preventDefault();
 
-              // Add the nonce to the form and submit
-              document.querySelector('#nonce').value = payload.nonce;
-              form.submit();
+                instance.requestPaymentMethod(function (err, payload) {
+                    if (err) {
+                        console.log('Request Payment Method Error', err);
+                        return;
+                    }
+
+                    // Add the nonce to the form and submit
+                    document.querySelector('#nonce').value = payload.nonce;
+                    form.submit();
+                });
             });
-          });
         });
     </script>
-    </body>
+</body>
+
 </html>
