4d147650a3fef9ebcaed90eea1455c4db6d10af8
Adam Huang <adamhut@gmail.com>
2019-06-18 07:22:12 -0700

    wip

---
+++ resources/js/components/SearchFilters.vue
@@ -0,0 +1,118 @@
+<template>
+    <section class="bg-gray-800 ">
+        <div class="flex justify-between py-3 px-4">
+            <div class="relative">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3">
+                    <svg class="h-6 w-6 fill-current text-gray-600" viewBox="0 0 24 24" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 0 1-2.12 2.12l-3-3a1.5 1.5 0 0 1-.44-1.14l-1.1-1.1a8 8 0 1 1 1.41-1.41l.01-.01zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                    </svg>
+                </div>
+                <input
+                    class="bg-gray-900 outline-none focus:outline-none focus:bg-white focus:text-gray-900  text-white rounded pl-10 pr-4 py-2 "
+                    type="text" placeholder="Search by keywords">
+            </div>
+            <button class="inline-flex bg-gray-700 rounded-lg shadow pr-2 pl-3 hover:bg-gray-600 focus:shadow-outline">
+                <svg class="h-6 w-6 fill-current text-gray-500" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M3 6a1 1 0 0 1 1-1h16a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1zm3 6a1 1 0 0 1 1-1h10a1 1 0 1 1 0 2H7a1 1 0 0 1-1-1zm4 5a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-4z" />
+                    </svg>
+                <span class="text-white font-medium ml-1">Filters</span>
+            </button>
+        </div>
+        <div>
+            <form action="">
+                <fieldset>
+                    <div>
+                        <label for="">
+                            <span class="text-gray-600">Bedrooms</span>
+                            <select name="" id="" class="form-select">
+                                <option value="1">4</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div>
+                        <label for="">
+                            <span>Bathrooms</span>
+                            <select name="" id=""  class="form-select">
+                                <option value="1">4</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div>
+                        <label for="">
+                            <span>Price Range</span>
+                            <select name="" id="" class="form-select">
+                                <option value="1">Up to $2,000/wk</option>
+                            </select>
+                        </label>
+                    </div>
+                </fieldset>
+                <fieldset>   
+                    <legend>Property Type</legend>  
+                    <label for="">
+                        <input   class="form-radio" type="radio" name="propertyType" value="house">
+                        House
+                    </label> 
+                    <label for="">
+                        <input   class="form-radio" type="radio" name="propertyType" value="apartment">
+                        Apartment
+                    </label> 
+                    <label for="">
+                        <input   class="form-radio" type="radio" name="propertyType" value="loft">
+                        Loft
+                    </label> 
+                    <label for="">
+                        <input   class="form-radio" type="radio" name="propertyType" value="townhouse">
+                        Townhouse
+                    </label>         
+                </fieldset>
+                <fieldset>   
+                    <legend>Amenities</legend>  
+                    <label for="">
+                        <input   class="form-checkbox" type="checkbox" name="amenities[]" value="balcony">
+                        Balcony
+                    </label> 
+                    <label for="">
+                        <input   class="form-checkbox" type="checkbox" name="amenities[]" value="airConditioning">
+                        Air Conditioning
+                    </label> 
+                    <label for="">
+                        <input   class="form-checkbox" type="checkbox" name="amenities[]" value="pool">
+                        Pool
+                    </label> 
+                    <label for="">
+                        <input   class="form-checkbox" type="checkbox" name="amenities[]" value="beach">
+                        Beach
+                    </label> 
+                    <label for="">
+                        <input   class="form-checkbox" type="checkbox" name="amenities[]" value="petFriendly">
+                        Pet Friendly
+                    </label> 
+                    <label for="">
+                        <input   class="form-checkbox" type="checkbox" name="amenities[]" value="kidFriendly">
+                        Kid  Friendly
+                    </label>  
+                    <label for="">
+                        <input   class="form-checkbox" type="checkbox" name="amenities[]" value="parking">
+                        parking
+                    </label>         
+                </fieldset>
+                <div>
+                    <button>Update Result</button>
+                </div>
+            </form>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/workcation.js
@@ -0,0 +1,35 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import PortalVue from 'portal-vue';
+
+Vue.use(PortalVue);
+// import VueFuse from 'vue-fuse'
+
+// Vue.use(VueFuse)
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+import SiteHeader from './components/SiteHeader';
+import SearchFilters from './components/SearchFilters';
+
+Vue.component('SiteHeader', SiteHeader);
+Vue.component('SearchFilters', SearchFilters);
+// Vue.component("DatePicker", require("./components/advanced-vue/DatePicker.vue"));
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        date: '2018-04-12'
+    },
+
+});
