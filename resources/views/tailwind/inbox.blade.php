6d78022e7dc46419be43e0c301b21d43f9daf1ad
Adam Huang <adamhut@gmail.com>
2019-11-08 06:51:08 -0800

    stop at 1:09:47

--- resources/views/tailwind/inbox.blade.php
+++ resources/views/tailwind/inbox.blade.php
@@ -65,9 +65,76 @@
 
         </header>
         <div class="flex-1 flex-shrink-0 flex">
-            <div class="w-64 bg-gray-100">
+            <div class="w-64 p-6 bg-gray-100">
                 <nav>
-                    
+                    <h2 class="text-sm uppercase font-semibold text-gray-600 tracking-wide">
+                        Mail Boxes
+                    </h2>
+                    <div class="mt-3">
+                        <a href="#" class="-mx-3 px-3 py-2 text-sm font-medium flex items-center justify-between bg-gray-200 rounded-lg">
+                            <span class="inline-flex">
+                            <svg class="fill-current w-6 h-6 text-gray-700" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5 3h14a2 2 0 012 2v14a2 2 0 01-2 2H5a2 2 0 01-2-2V5c0-1.1.9-2 2-2zm0 10v6h14v-6h-2.38l-1.45 2.9a2 2 0 01-1.79 1.1h-2.76a2 2 0 01-1.8-1.1L7.39 13H5zm14-2V5H5v6h2.38a2 2 0 011.8 1.1l1.44 2.9h2.76l1.45-2.9a2 2 0 011.79-1.1H19z"/>
+    </svg>
+                                <span class="ml-2 text-gray-900 ">Inbox</span>
+                            
+                            </span>
+                            <span class="inline-block w-9 text-center py-1 text-xs leading-none font-semibold  text-gray-700 bg-gray-300  rounded-full ">6</span>
+                        </a>
+                        <a href="#" class="-mx-3 mt-2 px-3 py-2 text-sm font-medium flex items-center justify-between hover:bg-gray-200 rounded-lg">
+                            <span class="inline-flex">
+                            <svg class="fill-current w-6 h-6 text-gray-500" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5 3h14a2 2 0 012 2v14a2 2 0 01-2 2H5a2 2 0 01-2-2V5c0-1.1.9-2 2-2zm0 10v6h14v-6h-2.38l-1.45 2.9a2 2 0 01-1.79 1.1h-2.76a2 2 0 01-1.8-1.1L7.39 13H5zm14-2V5H5v6h2.38a2 2 0 011.8 1.1l1.44 2.9h2.76l1.45-2.9a2 2 0 011.79-1.1H19z"/>
+    </svg>
+                                <span class="ml-2 text-gray-700 ">Flagged</span>
+                            
+                            </span>
+                        </a>
+                        <a href="#" class="-mx-3 mt-2 px-3 py-2 text-sm font-medium flex items-center justify-between hover:bg-gray-200 rounded-lg">
+                            <span class="inline-flex">
+                            <svg class="fill-current w-6 h-6 text-gray-500" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5 3h14a2 2 0 012 2v14a2 2 0 01-2 2H5a2 2 0 01-2-2V5c0-1.1.9-2 2-2zm0 10v6h14v-6h-2.38l-1.45 2.9a2 2 0 01-1.79 1.1h-2.76a2 2 0 01-1.8-1.1L7.39 13H5zm14-2V5H5v6h2.38a2 2 0 011.8 1.1l1.44 2.9h2.76l1.45-2.9a2 2 0 011.79-1.1H19z"/>
+    </svg>
+                                <span class="ml-2 text-gray-700 ">Drafts</span>
+                            
+                            </span>
+                            <span class="inline-block w-9 text-center py-1 text-xs leading-none font-semibold  text-gray-700 bg-gray-300  rounded-full ">2</span>
+                        </a>
+                        <a href="#" class="-mx-3 mt-2 px-3 py-2 text-sm font-medium flex items-center justify-between hover:bg-gray-200 rounded-lg">
+                            <span class="inline-flex">
+                            <svg class="fill-current w-6 h-6 text-gray-500" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5 3h14a2 2 0 012 2v14a2 2 0 01-2 2H5a2 2 0 01-2-2V5c0-1.1.9-2 2-2zm0 10v6h14v-6h-2.38l-1.45 2.9a2 2 0 01-1.79 1.1h-2.76a2 2 0 01-1.8-1.1L7.39 13H5zm14-2V5H5v6h2.38a2 2 0 011.8 1.1l1.44 2.9h2.76l1.45-2.9a2 2 0 011.79-1.1H19z"/>
+    </svg>
+                                <span class="ml-2 text-gray-700 ">Assigned</span>
+                            </span>
+                            <span class="inline-block w-9 text-center py-1 text-xs leading-none font-semibold  text-gray-700 bg-gray-300  rounded-full ">1</span>
+                        </a>
+                        <a href="#" class="-mx-3 mt-2 px-3 py-2 text-sm font-medium flex items-center justify-between hover:bg-gray-200 rounded-lg">
+                            <span class="inline-flex">
+                            <svg class="fill-current w-6 h-6 text-gray-500" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5 3h14a2 2 0 012 2v14a2 2 0 01-2 2H5a2 2 0 01-2-2V5c0-1.1.9-2 2-2zm0 10v6h14v-6h-2.38l-1.45 2.9a2 2 0 01-1.79 1.1h-2.76a2 2 0 01-1.8-1.1L7.39 13H5zm14-2V5H5v6h2.38a2 2 0 011.8 1.1l1.44 2.9h2.76l1.45-2.9a2 2 0 011.79-1.1H19z"/>
+    </svg>
+                                <span class="ml-2 text-gray-700 ">Closed</span>
+                            
+                            </span>
+                        </a>
+                        <a href="#" class="-mx-3 mt-2 px-3 py-2 text-sm font-medium flex items-center justify-between hover:bg-gray-200 rounded-lg">
+                            <span class="inline-flex">
+                            <svg class="fill-current w-6 h-6 text-gray-500" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5 3h14a2 2 0 012 2v14a2 2 0 01-2 2H5a2 2 0 01-2-2V5c0-1.1.9-2 2-2zm0 10v6h14v-6h-2.38l-1.45 2.9a2 2 0 01-1.79 1.1h-2.76a2 2 0 01-1.8-1.1L7.39 13H5zm14-2V5H5v6h2.38a2 2 0 011.8 1.1l1.44 2.9h2.76l1.45-2.9a2 2 0 011.79-1.1H19z"/>
+    </svg>
+                                <span class="ml-2 text-gray-700 ">Junk</span>
+                            </span>
+                        </a>
+                    </div>
+                    <h2 class="mt-8 text-sm uppercase font-semibold text-gray-600 tracking-wide">
+                       Folders
+                    </h2>
+                    <div class="mt-4">
+                        <a class="block text-gray-700 font-medium text-sm" href="#">Refunds</a>
+                        <a class="mt-4 block text-gray-700 font-medium text-sm" href="#">Discounts</a>
+                        <a class="mt-4 block text-gray-700 font-medium text-sm" href="#">Bugs</a>
+                    </div>
                 </nav>
             </div>
             <main class="bg-gray-200">
